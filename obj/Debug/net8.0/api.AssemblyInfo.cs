d56b35908e3c9f0ccfa58583f6bc97d4eca625bb
Angelica Mendoza <23090566@alumno.utmetropolitana.edu.mx>
2025-02-06 12:05:16 -0600

    updated https

--- obj/Debug/net8.0/api.AssemblyInfo.cs
+++ obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33649edd1cde503272d0c9fcf84200030148b12d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67dab6ff8481a92597b3204ba22178e5c7a3a5d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

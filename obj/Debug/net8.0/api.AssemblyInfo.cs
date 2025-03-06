69dacb2bbda56dcb1727335af6dc8d27c1f249f1
Angelica Mendoza <23090566@alumno.utmetropolitana.edu.mx>
2025-03-06 10:04:40 -0600

    new commit

--- obj/Debug/net8.0/api.AssemblyInfo.cs
+++ obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fccbfd7f2652e632b38632584520b3f4f44d17f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67110a93b50f9a844acd3d8b0bd47cdc0e31067d")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
